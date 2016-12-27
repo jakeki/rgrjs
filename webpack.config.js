49021ac049a1e18b66ddd860d6b939bd9caa3aac
Jarmo Backlund <jarmo.t.backlund@gmail.com>
2016-12-27 22:23:11 +0200

    after 1st chapter

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    entry: "./public/js/app.js",
+    entry: "./js/app.js",
     output: {
         path: __dirname + "/public",
         filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' 
+ //             query: { "presets": ["es2015", "react"] }         
+            }
+        ]
     }
 }
